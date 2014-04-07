98c6b094bb27551b3c528e59813f567064e933c5
natalia i karaivanova <nataliak@abv.bg>
2014-04-07 21:13:00 +0300

    ruby 2

--- html_puts.rb
+++ html_puts.rb
@@ -1,10 +1,8 @@
 # encoding: UTF-8
 bbegin ='<html>
  <head>
-</head>
-	<body>
-		<h1>
-			<title>моята страница</title>
+		<title>моята страница</title>
+		<meta charset="utf-8">	
 		<style type="text/css">
          body {
          background: url("images/background.jpg") fixed no-repeat center;
@@ -16,15 +14,23 @@ bbegin ='<html>
 <body>
 <h1>'
 	en_name = "natali"
+	en_middle = '</h1>
+				<h2 style="position:absolute; TOP:45px; Left:120px; WIGHT:400px HEIGHT:400px">Hello!</h2>
+				<p style="position:absolute; TOP:110px; Left:120px; WIGHT:400px HEIGHT:400px">
+				<strong>tralala, tralala</strong><p>
+				<p style="position:absolute; TOP:150px; Left:120px; WIGHT:400px HEIGHT:400px">wellcome to my b-day</p>'
+
+
 	bg_name = "натали"
-eend = '</h1>
-	</body>
-</html>'
+	bg_middle = '</h1>
+				<h2 style="position:absolute; TOP:45px; Left:120px; WIGHT:400px HEIGHT:400px">Здравейте!</h2>
+				<p style="position:absolute; TOP:110px; Left:120px; WIGHT:400px HEIGHT:400px"><strong>тралала, тралала</strong><p>
+				<p style="position:absolute; TOP:150px; Left:120px; WIGHT:400px HEIGHT:400px">на рожден ден ела</p>'
+	eend = '</body>
+		</html>'
 
-killbill = bbegin + en_name + eend
-
-
+killbill = bbegin + en_name + en_middle + eend
 File.write "C:\\Documents and Settings\\Natalia\\My Documents\\GitHub\\nataliika.github.io\\tintin.html",killbill
 
-killbill_bg = bbegin + bg_name + eend
+killbill_bg = bbegin + bg_name + bg_middle + eend
 File.write "C:\\Documents and Settings\\Natalia\\My Documents\\GitHub\\nataliika.github.io\\tintin_bg.html",killbill_bg
